386ce5608461b7ce232603f3ce3fefb49ec5be81
Lucy Ajumi Greg <amehajumi3@gmail.com>
2022-11-17 05:01:34 +0100

    added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import { useContext } from "react";
-import Header from "./components/Header";
-import Countries from "./pages/Countries";
-import ThemeBotton from "./ThemeBotton";
 import { ThemeContext } from "./ThemeContext";
-
-
 import AppRoute from './routes';
 
 
